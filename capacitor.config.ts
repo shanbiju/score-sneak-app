1e3dfc6b608144ce0f6307153a09eb11d7358571
shanbiju <shanbijuofficial@gmail.com>
2026-03-08 12:54:35 +0530

    feat: setup capacitor and github actions for APK build

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'com.shanbiju.scoresneak',
+    appName: 'Score Sneak',
+    webDir: 'dist',
+    bundledWebRuntime: false
+};
+
+export default config;
